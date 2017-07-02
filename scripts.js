8e37e83b33cfd4e1ad055152ed8feeb4b5ab652e
Malgorzata Groves <bromba1@tlen.pl>
2017-07-02 21:17:20 +0200

    new file for 12.6

---
+++ scripts.js
@@ -0,0 +1,33 @@
+//goly kod nie ostylowany, ale mam problem z jedna z funkcjonalnosci
+$(function() {
+	var url = 'https://restcountries.eu/rest/v1/name/';
+	var countriesList = $('#countries');
+	$('#search').click(searchCountries);
+	function searchCountries() {
+ 		var countryName = $('#country-name').val();
+	if(!countryName.length) countryName = 'Poland';
+	$.ajax({
+  		url: url + countryName,
+  		method: 'GET',
+  		success: showCountriesList
+  	});
+}
+	function showCountriesList(resp) {
+  		countriesList.empty();
+  		resp.forEach(function(item){
+   		$('<li>').text(item.name).appendTo(countriesList);
+   		$('<li>').html('<span>Capital: </span>' + item.capital).appendTo(countriesList);
+        $('<li>').html('<span>Land area: </span>' + item.area).appendTo(countriesList);
+        $('<li>').html('<span>Population: </span>' + item.population).appendTo(countriesList);
+        $('<li>').html('<span>Native Name: </span>' + item.nativeName).appendTo(countriesList);
+        $('<li>').html('<span>Region: </span>' + item.region).appendTo(countriesList);
+        //mam tutaj problem - bo regional bloc wydaje sie nie byc znajdowany mimo ze w API wystepuje???
+        $('<li>').html('<span>Regional Bloc: </span>' + item.regionalbloc).appendTo(countriesList);
+        $('<li>').html('<span>Calling Code: </span>' + "+" + item.callingCodes[0]).appendTo(countriesList);
+        $('<li>').html('<span>Currency: </span>' + item.currencies[0]).appendTo(countriesList);
+        $('<li>').html('<span>Language: </span>' + item.languages[0]).appendTo(countriesList);
+        $('<li>').html('<span>Alpha 3 Code: </span>' + item.alpha3Code).appendTo(countriesList);
+        $('<li>').html('<span>Timezone: </span>' + item.timezones).appendTo(countriesList);
+});
+}
+})
